5dca384e53584dfff91027f655a6c207f4b37962
varunjoshi27 <varunjoshi1127@gmail.com>
2025-09-08 02:18:00 +0530

    Created teh Folder for the Food Item

---
+++ Backend/src/models/food.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const foodSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  video: {
+    type: String,
+    required: true,
+  },
+});
