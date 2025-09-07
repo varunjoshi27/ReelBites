ba975fe0c452dd3fac20d28336a2d4f987b860b4
varunjoshi27 <varunjoshi1127@gmail.com>
2025-09-07 23:47:37 +0530

    Created the API Routes for the Food Partner

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -2,9 +2,16 @@ const express = require('express');
 const authController = require('../controllers/auth.controller');
 const router = express.Router();
 
-router.post('/user/register', authController.registerUser);
+// User auth APIs
 
+router.post('/user/register', authController.registerUser);
 router.post('/user/login', authController.loginUser);
 router.get('/user/logout', authController.logoutUser);
+
+// Food Partner auth APIs
+
+router.post('/food-partner/register', authController.registerFoodPartner);
+router.post('/food-partner/login', authController.loginFoodPartner);
+router.get('/food-partner/logout', authController.loginFoodPartner);
 
 module.exports = router;
