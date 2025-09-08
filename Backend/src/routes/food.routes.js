fcc5bbd7f24b608f9468cba7e8030964a664df55
varunjoshi27 <varunjoshi1127@gmail.com>
2025-09-08 22:44:36 +0530

    Created the Food route for the Food Database

---
+++ Backend/src/routes/food.routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+// This API is used to create the food item should be created in the Database
+
+/*   POST (/api/food/) {Protected } */
+router.post('/');
+
+module.export = router;
