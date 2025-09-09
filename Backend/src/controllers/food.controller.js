eddc3b1934c8f2faf3825c59b8f57dba7461af0a
varunjoshi27 <varunjoshi1127@gmail.com>
2025-09-09 20:05:57 +0530

    Added the Controller for the Food Item

--- Backend/src/controllers/food.controller.js
+++ Backend/src/controllers/food.controller.js
@@ -2,6 +2,7 @@ const foodModel = require('../models/food.model');
 
 async function createFood(req, res) {
   console.log(req.foodpartner);
+  console.log('Food Item Added');
 }
 
 module.exports = {
